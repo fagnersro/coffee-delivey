bb9a54095948e17aed210b32041d49eaa9bef499
fagnersro <fagner.hg40@gmail.com>
2023-04-11 11:17:37 -0300

    Aplicattion structure - Page Success

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,8 +1,16 @@
+import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react'
+import Ilustration from '../../assets/Illustration.png'
 import {
+  CardDelivery,
+  CardForecast,
+  CardPayment,
   ContainerContentSuccess,
   ContentDow,
+  ContentImage,
   ContentSuccess,
+  I,
   SectionSuccess,
+  WrapperContentDow,
   WrapperSuccess,
 } from './style'
 
@@ -12,9 +20,50 @@ export default function Success() {
       <WrapperSuccess>
         <ContainerContentSuccess>
           <ContentSuccess>
-            <h1>Success</h1>
-            <ContentDow></ContentDow>
+            <h2>Uhu! Pedido confirmado</h2>
+            <p>Agora é só aguardar que logo o café chegará até você</p>
+            <ContentDow>
+              <WrapperContentDow>
+                <CardDelivery>
+                  <I color="purple">
+                    <MapPin weight="fill" />
+                  </I>
+                  <div>
+                    <span>
+                      Entrega em{' '}
+                      <strong>Rua João Daniel Martinelli, 102</strong> Farrapos
+                      - Porto Alegre, RS
+                    </span>
+                  </div>
+                </CardDelivery>
+
+                <CardForecast>
+                  <I color="yellow">
+                    <Timer weight="fill" />
+                  </I>
+                  <div>
+                    <span>
+                      Previsão de entrega <strong>20 min - 30 min</strong>
+                    </span>
+                  </div>
+                </CardForecast>
+
+                <CardPayment>
+                  <I color="darkYellow">
+                    <CurrencyDollar />
+                  </I>
+                  <div>
+                    <span>
+                      Pagamento na entrega <strong>Cartão de Crédito</strong>
+                    </span>
+                  </div>
+                </CardPayment>
+              </WrapperContentDow>
+            </ContentDow>
           </ContentSuccess>
+          <ContentImage>
+            <img src={Ilustration} alt="" />
+          </ContentImage>
         </ContainerContentSuccess>
       </WrapperSuccess>
     </SectionSuccess>
--- src/pages/Success/style.ts
+++ src/pages/Success/style.ts
@@ -1,7 +1,6 @@
 import { styled } from '../../styles'
 
 export const SectionSuccess = styled('section', {
-  border: '1px solid green',
   display: 'flex',
   justifyContent: 'center',
 
@@ -9,27 +8,155 @@ export const SectionSuccess = styled('section', {
 })
 
 export const WrapperSuccess = styled('div', {
-  border: '1px solid black',
-
   width: '100%',
   maxWidth: '69.622rem',
 })
 
 export const ContainerContentSuccess = styled('div', {
-  border: '1px solid red',
+  display: 'flex',
 
   marginTop: '5rem',
   height: '23.875rem',
 })
 
 export const ContentSuccess = styled('div', {
-  border: '1px solid purple',
   height: '100%',
   width: '32.875rem',
+
+  h2: {
+    color: '$yellowDark',
+    fontFamily: 'Baloo 2',
+    fontStyle: 'normal',
+    fontWeight: 800,
+    fontSize: '2rem',
+  },
+
+  p: {
+    color: '$baseSubtitle',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '1.25rem',
+    marginBottom: '2.813rem',
+  },
 })
 
 export const ContentDow = styled('div', {
-  border: '1px solid yellow',
+  border: '1px solid #DBAC2C',
   borderRadius: '6px 36px',
+
+  display: 'flex',
+  alignItems: 'center',
+
   height: '16.875rem',
+})
+
+export const WrapperContentDow = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '2rem',
+
+  marginLeft: '2.5rem',
+  height: '11.699rem',
+  width: '27.625rem',
+})
+
+export const I = styled('i', {
+  borderRadius: '100%',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  width: '2rem',
+  height: '2rem',
+
+  svg: {
+    fontSize: '1rem',
+    color: '$white',
+  },
+
+  variants: {
+    color: {
+      purple: {
+        backgroundColor: '$purple',
+      },
+      yellow: {
+        backgroundColor: '$yellow',
+      },
+      darkYellow: {
+        backgroundColor: '$yellowDark',
+      },
+    },
+  },
+})
+
+export const CardDelivery = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.75rem',
+
+  height: '2.625rem',
+  width: '100%',
+
+  div: {
+    height: '100%',
+    width: '19.375rem',
+  },
+  span: {
+    color: '$baseText',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '16px',
+  },
+})
+
+export const CardForecast = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.75rem',
+
+  height: '2.625rem',
+  width: '100%',
+
+  div: {
+    height: '100%',
+    width: '10.813rem',
+  },
+  span: {
+    color: '$baseText',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '16px',
+  },
+})
+
+export const CardPayment = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.75rem',
+
+  height: '2.625rem',
+  width: '100%',
+
+  div: {
+    height: '100%',
+    width: '11.813rem',
+  },
+
+  span: {
+    color: '$baseText',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '16px',
+  },
+})
+
+export const ContentImage = styled('div', {
+  margin: '6.100rem 0 0 5.875rem',
+  height: '18.313rem',
+  width: '30.75rem',
 })
